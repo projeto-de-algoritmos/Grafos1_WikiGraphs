dd1c1c2080e8b4ff6add2d71e0503704c6102861
Lucas Boaventura <lucasxboaventura@hotmail.com>
2022-11-21 23:39:27 -0300

    Finaliza frontend da aplicação
    Co-authored-by: Herick Portugues <herick.portugues@gmail.com>

--- wiki-graphs/src/App.js
+++ wiki-graphs/src/App.js
@@ -1,8 +1,14 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
-import data from './data/dump/titles_links.json';
+import data from './data/dump/10k.json';
+import findWikiPathWithBFS from './data/graph';
 
 function App() {
+  const titles = Object.keys(data).map((key, index) => ({
+    value: `${index}`,
+    label: key,
+  }));
+
   const [source, setSource] = useState('');
   const [destiny, setDestiny] = useState('');
 
@@ -11,13 +17,8 @@ function App() {
   const [destinyOptions, setDestinyOptions] = useState([]);
   const [chooseDestiny, setChooseDestiny] = useState('');
 
-  const [titles, setTitles] = useState(
-    Object.keys(data).map((key, index) => ({ value: `${index}`, label: key })),
-  );
-
-  useEffect(() => {
-    console.log({ source, destiny, chooseSource, chooseDestiny });
-  }, [source, destiny, chooseDestiny, chooseSource]);
+  const [path, setPath] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     if (source.length > 3) {
@@ -38,6 +39,23 @@ function App() {
       );
     } else setDestinyOptions([]);
   }, [destiny]);
+
+  const searchWikiPages = () => {
+    if (!chooseSource || !chooseDestiny) {
+      setError('Preencha os campos de Origem e Destino, por favor');
+      return;
+    }
+
+    const result = findWikiPathWithBFS(chooseSource, chooseDestiny);
+
+    if (result.length === 0) {
+      setError('Não foi encontrado nenhum caminho para essa busca... ):');
+      return;
+    }
+
+    setError('');
+    setPath(result);
+  };
 
   return (
     <div className="App">
@@ -61,6 +79,7 @@ function App() {
               options={sourceOptions}
               onInputChange={text => setSource(text)}
               onChange={option => setChooseSource(option.label)}
+              isClearable
             />
           </div>
           <div id="input-container">
@@ -78,9 +97,27 @@ function App() {
               options={destinyOptions}
               onInputChange={text => setDestiny(text)}
               onChange={option => setChooseDestiny(option.label)}
+              isClearable
             />
           </div>
         </div>
+
+        <button type="button" onClick={() => searchWikiPages()}>
+          Buscar o caminho
+        </button>
+
+        <div id="path-content">
+          {error !== '' ? (
+            <p id="error">{error}</p>
+          ) : (
+            path.map((p, idx) => {
+              return idx + 1 < path.length ? (
+                <p key={p} id="path">{`${path[idx]} -> ${path[idx + 1]}`}</p>
+              ) : null;
+            })
+          )}
+        </div>
+        {/* <p id="path">{path.join(' -> ')}</p> */}
       </section>
     </div>
   );
